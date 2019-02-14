7abe6991797345bc5764701f51f0128d13e62383
Meghna Joshi <ldeveloperl1985@gmail.com>
2019-02-14 18:31:05 +0530

    Update demo

--- demo/config/passport-setup.js
+++ demo/config/passport-setup.js
@@ -9,7 +9,10 @@ passport.deserializeUser((user, done) => done(null, user));
 passport.use(
   new OXDStrategy(keys.oxd, (req, accessToken, userInfo, done) => {
     if (accessToken.access_token) {
-      done(null, { id: userInfo.claims.sub[0], name: userInfo.claims.name[0], email: userInfo.claims.email[0] });
+      console.log('------ userinfo -----', userInfo, '----- accessToken ------', accessToken);
+      return done(null, { id: userInfo.claims.sub[0], name: userInfo.claims.name[0], email: userInfo.claims.email[0] });
     }
+
+    return done({ message: 'Failed to get access_token' }, null);
   })
 );
