192b9b0c462345ee4e91a8ecba0f82172a4e8615
Meghna Joshi <ldeveloperl1985@gmail.com>
2020-03-17 20:16:47 +0530

    Remove deprecated request dependency and use got dependency #3

--- demo/config/keys.js
+++ demo/config/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
   oxd: {
-    clientID: '@!0A71.4656.3B20.FF54!0001!A25D.7300!0008!2F97.98AA.9469.986C',
-    clientSecret: '435e9cfb-1eea-4e24-a5a2-d997c01808cd',
-    oxdID: 'f19c3b25-391a-4a83-a90f-eaae56f2efba',
+    clientID: '97bc2428-006f-4792-9e72-a0f1954e1e6f',
+    clientSecret: '84183877-b203-4d85-8f23-d81cdfad3c2d',
+    oxdID: 'ea84abf3-9f2c-4bd9-8aed-f2f925a7d697',
     issuer: 'https://gluu.local.org',
     oxdServer: 'https://localhost:8553',
-    redirect_uri: 'http://localhost:4200'
+    redirect_uri: 'https://localhost:4200/auth/gluu/redirect'
   }
 };
--- demo/config/passport-setup.js
+++ demo/config/passport-setup.js
@@ -10,7 +10,7 @@ passport.use(
   new OXDStrategy(keys.oxd, (req, accessToken, userInfo, done) => {
     if (accessToken.access_token) {
       console.log('------ userinfo -----', userInfo, '----- accessToken ------', accessToken);
-      return done(null, { id: userInfo.claims.sub[0], name: userInfo.claims.name[0], email: userInfo.claims.email[0] });
+      return done(null, { id: userInfo.sub, name: userInfo.name });
     }
 
     return done({ message: 'Failed to get access_token' }, null);
